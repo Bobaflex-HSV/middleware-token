211e168ef11c52fe2a87073d867e1b55d48e0dde
Jens Duray <bobaflex.hsv@gmail.com>
2020-05-05 13:10:38 +0200

    add /:token route

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const bodyParser  = require('body-parser');
+const port = 3000;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+const secure = (res, req, next) => {
+    console.log(res.json())
+}
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/:token', (req, res) => {
+    const {token} = req.params;
+    res.send('Hello token:' + token)
+}
+
+)
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
