16844db98d38b3a6d323077164853b2ee5bace36
Jens Duray <bobaflex.hsv@gmail.com>
2020-05-06 13:22:33 +0200

    finalize secure middleware

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({
 
 const secure = (req, res, next) => {
     const {token} = req.params
-    if (token.length) {
+    if (token && token.length) {
      res.send('There is a token! And this is what it says: ' + token);
     return;}
     else {
@@ -18,8 +18,8 @@ const secure = (req, res, next) => {
  }
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! I am a path to /'))
 
 app.get('/:token', secure);
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Server running and listening at http://localhost:${port}...`))
