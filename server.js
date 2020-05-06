ee0123e3f68d7b9c733fb6548a29debf0b15f374
Jens Duray <bobaflex.hsv@gmail.com>
2020-05-06 14:30:25 +0200

    add postgres database connection

--- server.js
+++ server.js
@@ -4,10 +4,21 @@ const bodyParser  = require('body-parser');
 const port = 3000;
 
 
+const { Pool} = require('pg');
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'token_authetication',
+  password: 'jx5kwch6',
+  port: 5432,
+});
+
+
 app.use(bodyParser.urlencoded({
     extended: true
   }));
 
+  // define 'secure' middleware for token verification
 const secure = (req, res, next) => {
     const {token} = req.params
     if (token && token.length) {
@@ -17,9 +28,12 @@ const secure = (req, res, next) => {
         res.sendStatus(403);}
  }
 
+// root endpoint definition
+app.get('/', (req, res) => { 
+  console.log(pool);
+  res.send('Hello World! I am a path to /')})
 
-app.get('/', (req, res) => res.send('Hello World! I am a path to /'))
-
+// token endpoint definition
 app.get('/:token', secure);
 
 app.listen(port, () => console.log(`Server running and listening at http://localhost:${port}...`))
